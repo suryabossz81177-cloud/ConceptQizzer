3877c51b9f7572574138c99055c9c53a2c3ca103
suryabossz81177-cloud <suryabossz81177@gmail.com>
2026-08-08 15:28:59 +0530

    Package 5A - Added Notes Database

---
+++ notesDatabase.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const NotesDatabase = {
+  chapters: {
+
+    "class6-maths-1": {
+      title: "Knowing Our Numbers",
+      description: "Learn about large numbers, place value, estimation and comparison.",
+      summary: "",
+      sections: [],
+      importantPoints: [],
+      keyTerms: []
+    },
+
+    "class6-maths-2": {
+      title: "Whole Numbers",
+      description: "",
+      summary: "",
+      sections: [],
+      importantPoints: [],
+      keyTerms: []
+    }
+
+  }
+};
