b13fecf63c7cd6269e736c3a891e10cb1f0ee832
suryabossz81177-cloud <suryabossz81177@gmail.com>
2026-08-08 15:44:10 +0530

    Add sections to 'Whole Numbers' in notesDatabase

--- notesDatabase.js
+++ notesDatabase.js
@@ -16,7 +16,72 @@ const NotesDatabase = {
       title: "Whole Numbers",
       description: "",
       summary: "",
-      sections: [],
+      sections: [
+{
+title: "Introduction",
+content: ""
+},
+{
+title: "History",
+content: ""
+},
+{
+title: "Need of Numbers",
+content: ""
+},
+{
+title: "Indian Number System",
+content: ""
+},
+{
+title: "International Number System",
+content: ""
+},
+{
+title: "Place Value",
+content: ""
+},
+{
+title: "Face Value",
+content: ""
+},
+{
+title: "Expanded Form",
+content: ""
+},
+{
+title: "Comparing Numbers",
+content: ""
+},
+{
+title: "Ascending Order",
+content: ""
+},
+{
+title: "Descending Order",
+content: ""
+},
+{
+title: "Estimation",
+content: ""
+},
+{
+title: "Rounding Off",
+content: ""
+},
+{
+title: "Applications",
+content: ""
+},
+{
+title: "Examples",
+content: ""
+},
+{
+title: "Practice Tips",
+content: ""
+}
+],
       importantPoints: [],
       keyTerms: []
     }
