8b37f38fe7e5f518601b36c49fd864c3a677257f
suryabossz81177-cloud <suryabossz81177@gmail.com>
2026-08-15 15:11:40 +0530

    Initialize science quiz database for class 6

---
+++ js/class6/science-quiz-database.js
@@ -0,0 +1,17 @@
+const quizDatabase = {
+  class6: {
+    science: {
+
+      "the-wonderful-world-of-science": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      }
+
+    }
+  }
+};
+
+window.quizDatabase = quizDatabase;
