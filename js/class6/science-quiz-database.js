de7ae9df45d34383a831a5362694b1f169669ed6
suryabossz81177-cloud <suryabossz81177@gmail.com>
2026-08-15 15:14:56 +0530

    Add new quiz categories to science quiz database

--- js/class6/science-quiz-database.js
+++ js/class6/science-quiz-database.js
@@ -8,6 +8,94 @@ const quizDatabase = {
         hard: [],
         tooHard: [],
         extreme: []
+      },
+
+      "diversity-in-the-living-world": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      },
+
+      "mindful-eating-a-path-to-a-healthy-body": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      },
+
+      "exploring-magnets": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      },
+
+      "measurement-of-length-and-motion": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      },
+
+      "materials-around-us": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      },
+
+      "temperature-and-its-measurement": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      },
+
+      "a-journey-through-states-of-water": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      },
+
+      "methods-of-separation-in-everyday-life": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      },
+
+      "living-creatures-exploring-their-characteristics": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      },
+
+      "nature-s-treasures": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
+      },
+
+      "beyond-earth": {
+        easy: [],
+        medium: [],
+        hard: [],
+        tooHard: [],
+        extreme: []
       }
 
     }
