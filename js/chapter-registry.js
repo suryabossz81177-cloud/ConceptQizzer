4823dd75301c5275c3498659bd0a882abd520498
suryabossz81177-cloud <suryabossz81177@gmail.com>
2026-08-24 18:09:43 +0530

    Add new chapter 'The Rise of Empires' to registry

--- js/chapter-registry.js
+++ js/chapter-registry.js
@@ -7,6 +7,15 @@
 
 window.ChapterRegistry = [
   {
+  id: "7-history-the-rise-of-empires",
+  class: 7,
+  subject: "History",
+  chapter: 5,
+  title: "The Rise of Empires",
+  file: "js/notes/class7/history/the-rise-of-empires.js",
+},
+  
+  {
   id: "7-history-new-beginnings-cities-and-states",
   class: 7,
   subject: "History",
