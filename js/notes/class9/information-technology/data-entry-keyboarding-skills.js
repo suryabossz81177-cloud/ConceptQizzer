ea89fe87829536252e34310919ae165662f7a606
suryabossz81177-cloud <suryabossz81177@gmail.com>
2026-08-30 20:14:39 +0530

    Update chapter ID for data entry skills

--- js/notes/class9/information-technology/data-entry-keyboarding-skills.js
+++ js/notes/class9/information-technology/data-entry-keyboarding-skills.js
@@ -1,5 +1,5 @@
 window.chapter = {
-  "id": "class9-it-data-entry-keyboarding-skills",
+  "id": "9-it-data-entry-keyboarding-skills",
   "class": 9,
   "subject": "Information Technology",
   "title": "Data Entry & Keyboarding Skills",
